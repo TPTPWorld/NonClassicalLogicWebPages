743036a3724792c48b159441a34a361f98150a8e
Tobias Gleißner <tobias.gleissner@fokus.fraunhofer.de>
2021-08-09 16:43:10 +0200

    added semantics class added stdout output for generated problems

--- Tooling/generateSemantics.py
+++ Tooling/generateSemantics.py
@@ -6,14 +6,7 @@ from pathlib import Path
 from typing import Iterable
 
 logging.basicConfig(level=logging.NOTSET)
-default_template = \
-"""thf({id},logic,(
-    $modal == [ 
-        $constants == {constants},
-        $quantification == {quantification},
-        $consequence == {consequence},
-        $modalities == {modalities}
-]))."""
+
 all_constants = (
     "$rigid",
     "$flexible",
@@ -36,31 +29,61 @@ all_modalities = (
     "$modal_system_S5",
 )
 
-def createSemantics(template:str=default_template, constants:Iterable[str]=all_constants, quantification:Iterable[str]=all_quantification, consequence:Iterable[str]=all_consequence, modalities:Iterable[str]=all_modalities):
+class Semantics:
+    default_template = \
+"""thf({id},logic,(
+    $modal == [ 
+        $constants == {constants},
+        $quantification == {quantification},
+        $consequence == {consequence},
+        $modalities == {modalities}
+]))."""
+    def __init__(self, constants, quantification, consequence, modalities):
+        self.constants = constants
+        self.quantification = quantification
+        self.consequence = consequence
+        self.modalities = modalities
+
+    def name(self) -> str:
+        return self.constants.replace("$","") + "_" + self.quantification.replace("$","") + "_" + self.consequence.replace("$","") + "_" + self.modalities.replace("$","")
+
+    def specification(self) -> str:
+        return Semantics.default_template.format(id=self.name(), constants=self.constants, quantification=self.quantification, consequence=self.consequence, modalities=self.modalities)
+
+def createMultipleSemantics(constants:Iterable[str]=all_constants, quantification:Iterable[str]=all_quantification, consequence:Iterable[str]=all_consequence, modalities:Iterable[str]=all_modalities):
     ret = []
-    for element in itertools.product(constants, quantification, consequence, modalities):
-        id = element[0].replace("$","") + "_" + element[1].replace("$","") + "_" + element[2].replace("$","") + "_" + element[3].replace("$","")
-        semantics = template.format(id=id, constants=element[0], quantification=element[1], consequence=element[2], modalities=element[3])
-        ret.append((id, semantics, ))
+    for e1,e2,e3,e4 in itertools.product(constants, quantification, consequence, modalities):
+        ret.append(Semantics(constants=e1, quantification=e2, consequence=e3, modalities=e4))
     return ret
 
-def writeProblem(problemPath:Path, relativeProblemPath:Path, outDir:Path, constants:Iterable[str]=all_constants, quantification:Iterable[str]=all_quantification, consequence:Iterable[str]=all_consequence, modalities:Iterable[str]=all_modalities):
-    semantics = createSemantics(constants=constants, quantification=quantification, consequence=consequence, modalities=modalities)
-    writeBasePath = (outDir / relativeProblemPath).parent
-    problem = problemPath.read_text()
-    for s in semantics: # s[0] is the id of the logic statement, s[1] the problem filename
-        writePath = writeBasePath / (relativeProblemPath.stem + "_" + s[0] + ".p")
-        writePath.parent.mkdir(parents=True, exist_ok=True)
-        writePath.write_text(s[1] + "\n\n" + problem)
+def createProblemWithSemantics(semantics:Semantics, problem:str):
+    return semantics.specification() + "\n\n" + problem
 
-def writeDirectory(inDir:Path, outDir:Path, constants:Iterable[str]=all_constants, quantification:Iterable[str]=all_quantification, consequence:Iterable[str]=all_consequence, modalities:Iterable[str]=all_modalities):
+def stdoutProblem(semantics:Semantics, problem:str, filename:str):
+    print("%--------------------------------------------------------")
+    print("%---Problem: " + filename + " with semantics " + semantics.name() + "\n")
+    print(createProblemWithSemantics(semantics=semantics, problem=problem))
+
+def outProblem(problemPath:Path, relativeProblemPath:Path, outDir:Path, constants:Iterable[str]=all_constants, quantification:Iterable[str]=all_quantification, consequence:Iterable[str]=all_consequence, modalities:Iterable[str]=all_modalities):
+    semanticsList = createMultipleSemantics(constants=constants, quantification=quantification, consequence=consequence, modalities=modalities)
+    problem = problemPath.read_text()
+    for s in semanticsList: # s[0] is the id of the logic statement, s[1] the problem filename
+        if outDir: # write to disk
+            writeBasePath = (outDir / relativeProblemPath).parent
+            writePath = writeBasePath / (relativeProblemPath.stem + "_" + s.name() + ".p")
+            writePath.parent.mkdir(parents=True, exist_ok=True)
+            writePath.write_text(createProblemWithSemantics(s, problem))
+        else: # write to stdout
+            stdoutProblem(semantics=s, problem=problem, filename=str(relativeProblemPath))
+
+def outDirectory(inDir:Path, outDir:Path, constants:Iterable[str]=all_constants, quantification:Iterable[str]=all_quantification, consequence:Iterable[str]=all_consequence, modalities:Iterable[str]=all_modalities):
     for p in inDir.rglob("*.p"):
-        writeProblem(problemPath=p, relativeProblemPath=p.relative_to(inDir), outDir=outDir, constants=constants, quantification=quantification, consequence=consequence, modalities=modalities)
+        outProblem(problemPath=p, relativeProblemPath=p.relative_to(inDir), outDir=outDir, constants=constants, quantification=quantification, consequence=consequence, modalities=modalities)
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("inPath", help="input path (file or directory)")
-    parser.add_argument("outPath", help="output path (file if inPath is a file and only on semantics specification is chosen. Otherwise directory)")
+    parser.add_argument("--outPath", help="output path (file if inPath is a file and only on semantics specification is chosen. Otherwise directory)")
     parser.add_argument("--constants", help="comma-separated values for constants")
     parser.add_argument("--quantification", help="comma-separated values for quantification")
     parser.add_argument("--consequence", help="comma-separated values for consequence")
@@ -71,11 +94,13 @@ def parse_args():
 def main():
     args = parse_args()
     inPath = Path(args.inPath)
-    outPath = Path(args.outPath)
+    outPath = None
     constants = all_constants
     quantification = all_quantification
     consequence = all_consequence
     modalities = all_modalities
+    if args.outPath:
+        outPath = Path(args.outPath)
     if args.constants:
         constants = args.constants.split(",")
         if len(constants) < 1:
@@ -105,19 +130,23 @@ def main():
     # output: one file (one semantic)
     if inPath.is_file() and len(constants) == 1 and len(quantification) == 1 and len(consequence) == 1 and len(modalities) == 1:
         logging.info("Processing one file: " + str(inPath) + " with one semantics specification.")
-        semantics = createSemantics(constants=constants, quantification=quantification, consequence=consequence, modalities=modalities)
+        semanticsList = createMultipleSemantics(constants=constants, quantification=quantification, consequence=consequence, modalities=modalities)
+        semantics = semanticsList[0]
         problem = inPath.read_text()
-        outPath.write_text(semantics[0][1] + "\n\n" + problem)
+        if outPath:
+            outPath.write_text(createProblemWithSemantics(semantics=semantics, problem=problem))
+        else:
+            stdoutProblem(semantics=semantics, problem=problem, filename=inPath.name)
     # input one file
-    # output: multiple files (multiple semantics
+    # output: multiple problems (multiple semantics)
     elif inPath.is_file() and (len(constants) > 1 or len(quantification) > 1 or len(consequence) > 1 or len(modalities) > 1):
         logging.info("Processing one file: " + str(inPath) + " with multiple semantics specification.")
-        writeProblem(problemPath=inPath, relativeProblemPath=Path(inPath.name), outDir=outPath, constants=constants, quantification=quantification, consequence=consequence, modalities=modalities)
+        outProblem(problemPath=inPath, relativeProblemPath=Path(inPath.name), outDir=outPath, constants=constants, quantification=quantification, consequence=consequence, modalities=modalities)
     # input: directory
     # output: directory
     elif inPath.is_dir():
         logging.info("Processing directory " + str(inPath) + ".")
-        writeDirectory(inDir=inPath, outDir=outPath, constants=constants, quantification=quantification, consequence=consequence, modalities=modalities)
+        outDirectory(inDir=inPath, outDir=outPath, constants=constants, quantification=quantification, consequence=consequence, modalities=modalities)
     else:
         logging.error("Oops, something went wrong.")
         sys.exit(1)
